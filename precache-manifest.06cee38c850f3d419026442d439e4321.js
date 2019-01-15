e8c227832a1ade64dc511d90ddef4913a34227f1
Elijah Quartey <equartey@Elijahs-MacBook-Pro.local>
2019-01-14 19:15:40 -0700

    Updates

---
+++ precache-manifest.06cee38c850f3d419026442d439e4321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f1cbe1dc1f61194c61d",
+    "url": "/shopify-challenge/static/js/runtime~main.3f1cbe1d.js"
+  },
+  {
+    "revision": "673be26dce226b24ed14",
+    "url": "/shopify-challenge/static/js/main.673be26d.chunk.js"
+  },
+  {
+    "revision": "852522e27cd5953d3e25",
+    "url": "/shopify-challenge/static/js/1.852522e2.chunk.js"
+  },
+  {
+    "revision": "673be26dce226b24ed14",
+    "url": "/shopify-challenge/static/css/main.1955e690.chunk.css"
+  },
+  {
+    "revision": "c7a31c953577db736f5de0fe941dc933",
+    "url": "/shopify-challenge/index.html"
+  }
+];
